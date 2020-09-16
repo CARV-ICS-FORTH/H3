d623a53d1a22541d473e8ba2d7dfdd05975bc907
Antony Chazapis <chazapis@ics.forth.gr>
2020-09-16 11:32:14 +0300

    Add missing license headers

--- h3lib/h3lib.c
+++ h3lib/h3lib.c
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #include "common.h"
 #include "util.h"
 #include "url_parser.h"
